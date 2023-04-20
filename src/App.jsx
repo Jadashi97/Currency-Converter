42838b74e1048aa629ebf94508e9bb1181e12dd0
jadashi97 <njadauniversal23@yahoo.com>
2023-04-19 22:28:30 -0500

    (feat): addded tailwind css to style the app

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,7 @@ function App() {
   return (
     <div className="App">
       {/* change this to my own logo for the currency converter  */}
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Currency Converter</h1>
+      <h1 className="text-5xl font-bold px-4 py-4">Currency Converter</h1>
       <div>
       <label>
         Amount:
