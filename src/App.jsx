6020b2b12ee0876f23fad12a2a7a97e4cd45ed70
jadashi97 <njadauniversal23@yahoo.com>
2023-04-20 08:25:11 -0500

    (feat): added javascript code to invoke the state and change. (fix): add css and integrate the  api

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+const api_key = "";
+
+const base_url = ``;
+
+console.log(base_url);
+
 function App() {
-  const [count, setCount] = useState(0)
+    const [amount, setAmount] = useState(0);
+    const [fromCurrency, setFromCurrency] = useState('USD');
+    const [toCurrency, setToCurrency] = useState("SSP");
+    const [exhangeRate, setExchageRate] = useState(null);
+    const [convertedAmount, setConvertedAmount] = useState(0);
 
-  return (
-    <div className="App">
-      {/* change this to my own logo for the currency converter  */}
-      <h1 className="text-5xl font-bold px-4 py-4">Currency Converter</h1>
-      <div>
-      <label>
-        Amount:
-        <input type="number" />
-      </label>
-      <label>
-        From:
-        <select>
-          <option value="USD">USD</option>
-          <option value="EUR">EUR</option>
-          <option value="GBP">GBP</option>
-          {/* add more currencies here */}
-        </select>
-      </label>
-      <label>
-        To:
-        <select>
-          <option value="USD">USD</option>
-          <option value="EUR">EUR</option>
-          <option value="GBP">GBP</option>
-          <option value="SSP">SSP</option>
-          <option value="UGX">UGX</option>
-          {/* add more currencies here */}
-        </select>
-      </label>
-      <div>
-        Converted Amount:
+    useEffect(() => {
+      async function fetchExhangeRate(){
+        const response = await fetch()
+      }
+    })
+
+    const handleAmountChange = (e) => {
+      setAmount(parseFloat(e.target.value));
+    }
+
+    const handleFromCurrencyConverter = (e)=> {
+      setFromCurrency(e.target.value);
+    }
+
+    const handleToCurrencyConverter = () => {
+      setToCurrency(e.target.value);
+    }
+
+    return (
+      <div className="App">
+        {/* change this to my own logo for the currency converter  */}
+        <h1 className="text-5xl font-bold px-4 py-4">Currency Converter</h1>
+        <div>
+        <label>
+          Amount:
+          <input 
+            value={amount}
+            onChange={handleAmountChange}
+            type="number" 
+            name="currency" 
+            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset" 
+          />
+        </label>
+        <label>
+          From:
+          <select value={fromCurrency} onChange={handleFromCurrencyConverter} className='py-5 px-4'>
+            <option value="USD">USD</option>
+            <option value="EUR">EUR</option>
+            <option value="GBP">GBP</option>
+            {/* add more currencies here */}
+          </select>
+        </label>
+        <label>
+          To:
+          <select value={toCurrency} onChange={handleToCurrencyConverter} className='px-4'>
+            <option value="SSP">SSP</option>
+            <option value="EUR">EUR</option>
+            <option value="GBP">GBP</option>
+            <option value="UGX">UGX</option>
+            {/* add more currencies here */}
+          </select>
+        </label>
+        <div>
+          Converted Amount: {convertedAmount.toFixed(2)} {toCurrency}
+        </div>
       </div>
-    </div>
 
-    </div>
-  )
+      </div>
+    )
 }
 
 export default App
